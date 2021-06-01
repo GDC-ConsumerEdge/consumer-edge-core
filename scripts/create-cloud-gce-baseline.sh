999f949af779f01fdb41bc4de221e5996e7f5646
mike-ensor <mike@ensor.cc>
2021-06-01 14:21:50 +0000

    Resolve "Develop Automated VMs intallation KubeVirt"

--- scripts/create-cloud-gce-baseline.sh
+++ scripts/create-cloud-gce-baseline.sh
@@ -7,13 +7,15 @@
 unset GCE_COUNT
 unset REGION_OVERRIDE
 unset STARTING_INDEX
+unset PREEMPTIBLE_OPTION
 
-while getopts 'c:s:r:': option
+while getopts 'c:s:r:p:': option
 do
     case "${option}" in
-        c) GCE_COUNT=${OPTARG};;
+        c) GCE_COUNT="${OPTARG}";;
         s) STARTING_INDEX="${OPTARG}";;
         r) REGION_OVERRIDE="${OPTARG}";;
+        p) PREEMPTIBLE_OPTION="--preemptible";;
     esac
 done
 
@@ -21,9 +23,12 @@ usage()
 {
   echo "Usage: $0 -c 1
             [ -r us-west1 ]
-            [ -s 1 ]"
+            [ -s 1 ]
+            [ -p true ]"
   exit 2
 }
+
+echo "PREEMPT: Main -> $PREEMPTIBLE_OPTION"
 
 if [[ -z "${GCE_COUNT}" || ${GCE_COUNT} -le 0 ]]; then
     echo "Missing count variable"
@@ -45,6 +50,15 @@ source ./gce-helper.vars
 setup_init_bucket
 
 copy_init_script
+
+# enable any services needed
+gcloud services enable secretmanager.googleapis.com
+
+# setup default compute to view secrets
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+    --role="roles/secretmanager.secretAccessor"
 
 # Store the SSH pub key as a secret
 store_public_key_secret ${SSH_PUB_KEY_LOCATION}
