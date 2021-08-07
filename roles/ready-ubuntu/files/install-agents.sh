606ac3e192da8b7fdad12269270e88128516a7de
Mike Ensor <ensor@google.com>
2021-08-06 20:38:22 -0700

    partially through the first pass

--- roles/ready-ubuntu/files/install-agents.sh
+++ roles/ready-ubuntu/files/install-agents.sh
@@ -3,14 +3,16 @@ service google-cloud-ops-agent status
 if [[ "$?" !=  "0" ]] ; then
   service google-cloud-ops-agent | grep inactive
   if [[ "$?" ==  "1" ]] ; then
-    # download script file 
+    # download script file
     curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
     # Make the script file executable
     chmod +x add-google-cloud-ops-agent-repo.sh
     # Run the script file
     bash add-google-cloud-ops-agent-repo.sh --also-install
-    # Restart the service 
+    # Restart the service
     service google-cloud-ops-agent restart
   fi
 fi
- 
+
+Status of 'google-cloud-ops-agent'
+If not installed, then run "curl" and "bash" script
