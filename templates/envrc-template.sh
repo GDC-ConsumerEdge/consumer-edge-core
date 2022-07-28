f03b7957b651edb296e8936fc081930177cd9905
Mike Ensor <ensor@google.com>
2022-07-28 12:38:47 -0700

    setting root-repo
    Change-Id: I9cf333a6fd3d9ef56523f03c22f2ce889bf2fe2d

--- templates/envrc-template.sh
+++ templates/envrc-template.sh
@@ -2,27 +2,26 @@
 # GSA Key used for provisioning (result of running ./scripts/create-primary-gsa.sh)
 export LOCAL_GSA_FILE=$(pwd)/build-artifacts/consumer-edge-gsa.json
 # GCP Project ID
-export PROJECT_ID="### GCP PROJECT ID ###"
+export PROJECT_ID="###__GCP_PROJECT_ID__###"
+# Bucket to store cluster snapshot information
+export SNAPSHOT_GCS="###__GCP_PROJECT_ID__###-cluster-snapshots"
 
-# GCP Project Region
+# GCP Project Region (Adjust as desired)
 export REGION="us-west1"
-# GCP Project Zone
+# GCP Project Zone (Adjust as desired)
 export ZONE="us-west1-b"
 
 # (Optional, typically workshop only) Assigned name/number. This is the ACM Cluster object's name (needs to be unique across fleet)
-export CLUSTER_ACM_NAME="location-1"
+# export CLUSTER_ACM_NAME="location-1"
 
 # Gitlab Personal Access Token credentials (generated in Quick Start step 2)
-#export SCM_TOKEN_USER="### Repo Login Name ###"
-#export SCM_TOKEN_TOKEN="### Repo PAT Token ###"
+export SCM_TOKEN_USER="###_Repo_Login_Name_###"
+export SCM_TOKEN_TOKEN="###_Repo_PAT_Token_###"
 
 # Default Root Repo setup for multiple locations
-export ROOT_REPO_URL="https://gitlab.com/gcp-solutions-public/retail-edge/root-repo-edge-workshop.git"
+export ROOT_REPO_URL="https://gitlab.com/gcp-solutions-public/retail-edge/root-repo-public-template.git"
 export ROOT_REPO_BRANCH="main"
 export ROOT_REPO_DIR="/config"
-
-# Bucket to store cluster snapshot information
-export SNAPSHOT_GCS="${PROJECT_ID}-cluster-snapshots"
 
 # OIDC Configuration (off by default)
 export OIDC_CLIENT_ID="" # Optional, requires GCP API setup work
